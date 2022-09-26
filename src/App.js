7cedd228b63fbc254bb2e8ed37cd2f0c0a783c92
youngdima03 <106612329+youngdima03@users.noreply.github.com>
2022-09-26 18:48:37 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Movie from './components/Movie';
+import { dummy } from './movieDummy';
+
+function App() {
+  return (
+    <div>
+      <div className='app-container'>
+        {
+          dummy.results.map((item) => {
+            return (
+              <Movie
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+                />
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
